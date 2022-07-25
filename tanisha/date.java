370c9075ff7861453e35d02e50676feb23300c27
kalashjain1010 <77771443+kalashjain1010@users.noreply.github.com>
2022-07-25 23:17:31 +0530

    Delete tanisha directory

--- tanisha/date.java
+++
@@ -1,8 +0,0 @@
-import java.time.*; // import the LocalDate class
-
-public class date {
-  public static void main(String[] args) {
-    LocalDate myObj = LocalDate.now(); // Create a date object
-    System.out.println(myObj); // Display the current date
-  }
-}
--- tanisha/first.java
+++
@@ -1,6 +0,0 @@
-public class first {
-    public static void main(String[] args) {
-      int a = (int)(Math.random() );  // 0 to 100
-      System.out.println(a);
-    }
-  }
