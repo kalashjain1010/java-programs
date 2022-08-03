d33a14a26e92c6ea887386200cd99f5c7a7cfa5e
kalashjain1010 <77771443+kalashjain1010@users.noreply.github.com>
2022-08-03 17:00:30 +0530

    Add files via upload

---
+++ pattern question/normal.java
@@ -0,0 +1,19 @@
+public class normal {
+    public static void main(String[] args) {
+
+        int n = 3;
+        int m = 4;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 || i == n || j == 0 || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+}
